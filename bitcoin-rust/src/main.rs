662f2a914c2fcc0f6a0051e1edb5554d0d6b9792
Jkrish1011 <jayakrishnanashok@gmail.com>
2025-08-29 11:47:25 +0530

    working with miniscript

--- bitcoin-rust/src/main.rs
+++ bitcoin-rust/src/main.rs
@@ -4,6 +4,9 @@ use bitcoin::{Network, Address, XOnlyPublicKey};
 // use secp256k1::{Secp256k1, KeyPair, rand::rngs::OsRng};
 use secp256k1::{Secp256k1, Keypair};
 use rand::rngs::OsRng;
+use std::str::FromStr;
+
+use miniscript;
 
 
 fn main() {
@@ -22,4 +25,21 @@ fn main() {
     println!("Internal PK: {}", internal_pk);
     println!("Alice PK: {}", alice_pk);
     println!("Bob PK: {}", bob_pk);
+
+    let desc = miniscript::Descriptor::<miniscript::bitcoin::PublicKey>::from_str("\
+        sh(wsh(or_d(\
+        c:pk_k(020e0338c96a8870479f2396c373cc7696ba124e8635d41b0ea581112b67817261),\
+        c:pk_k(0250863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352)\
+        )))\
+    ").unwrap();
+
+    println!("Descriptor: {}", desc);
+
+    // Derive the P2SH address
+    assert_eq!(desc.address(miniscript::bitcoin::Network::Bitcoin).unwrap().to_string(), "3CJxbQBfWAe1ZkKiGQNEYrioV73ZwvBWns");
+
+    assert!(desc.sanity_check().is_ok());
+    // assert_eq!(desc.max_weight_to_satisfy().unwrap().to_wu(), 288);
+    println!("Max weight to satisfy: {}", desc.max_weight_to_satisfy().unwrap().to_wu());
+
 }
